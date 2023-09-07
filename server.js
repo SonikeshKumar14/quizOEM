77cb19dd0f40ab80733f5bd815ca1cf059ec4cfe
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-09-07 19:15:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const examRoute = require('./routes/Exam')
 require('dotenv').config()
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(bodyParser.json())
 
 mongoose.connect(process.env.DATABASE_ACCESS).then(data => {
